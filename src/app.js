003f7ec3e56f203e00db588099788a09bbfb7bc2
Tal Prokish <prokish@cmadmins-MacBook-Pro.local>
2021-06-06 15:09:00 +0300

    added dotenv

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv  from "dotenv";
 
 import { router } from './routes';
+
+dotenv.config();
 
 const app = express();
 
